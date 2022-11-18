c4d3973bddd71eec38386ea9473250e28c5c3546
Роман Худенко <damage----------inc@yandex.ru>
2022-11-18 14:56:11 +0400

    Added allure reporting system.

--- src/test/java/ru/netology/card/FormTest.java
+++ src/test/java/ru/netology/card/FormTest.java
@@ -1,6 +1,10 @@
 package ru.netology.card;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -13,6 +17,16 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
 public class FormTest {
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
     @BeforeEach
     public void openBrowser() {
         open("http://localhost:9999/");
